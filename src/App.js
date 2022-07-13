a4bd72cac8b74f9e285d4257ec7e04bd9db01f85
홍현아 <hahong@thekgolf.com>
2022-07-13 10:12:48 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 //Animaiton 사용방법
-import styled, { keyframes } from "styled-components";
+import styled, { keyframes } from 'styled-components';
+
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
 
 const Wrapper = styled.section`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${(props) => props.theme.backgroundColor};
 `;
 
 const rotateAnimaiton = keyframes`
@@ -21,43 +26,44 @@ const rotateAnimaiton = keyframes`
 `;
 
 const Box = styled.div`
-  height: 200px;
-  width: 200px;
-  background-color: tomato;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  animation: ${rotateAnimaiton} 1s linear infinite;
+    height: 200px;
+    width: 200px;
+    background-color: tomato;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    animation: ${rotateAnimaiton} 1s linear infinite;
 
-  ${Emoji}:hover {
-    font-size: 100px;
-  }
+    ${Emoji}:hover {
+        font-size: 100px;
+    }
 `;
 
 //컴포넌트를 따로 빼서 의존도를 낮추면 tag가 어떤 것이든 신경쓰는 게 줄어듦
 const Emoji = styled.span`
-  font-size: 36px;
+    font-size: 36px;
 
-  &:hover {
-    font-size: 60px;
-  }
+    &:hover {
+        font-size: 60px;
+    }
 
-  &:active {
-    opacity: 0;
-  }
+    &:active {
+        opacity: 0;
+    }
 `;
 
 function App() {
-  return (
-    <Wrapper>
-      <Box>
-        {/* 컴포넌트 안에서 selector 된 Emoji */}
-        <Emoji>😱</Emoji>
-      </Box>
-      {/* 원래 Emoji */}
-      <Emoji>🤤</Emoji>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Title>HELLO</Title>
+            <Box>
+                {/* 컴포넌트 안에서 selector 된 Emoji */}
+                <Emoji>😱</Emoji>
+            </Box>
+            {/* 원래 Emoji */}
+            <Emoji>🤤</Emoji>
+        </Wrapper>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const darkTheme = {
+    textColor: 'whitesmoke',
+    backgroundColor: '#111',
+};
+
+const lightTheme = {
+    textColor: '#111',
+    backgroundColor: 'whitesmoke',
+};
+//제일 최상단을 ThemeProvider로 감싸준 다음, Theme을 부여한다.
+//각 컴포넌트에서 props를 통해 theme 사용ㄴ
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <ThemeProvider theme={darkTheme}>
+            <App />
+        </ThemeProvider>
+    </React.StrictMode>
 );
