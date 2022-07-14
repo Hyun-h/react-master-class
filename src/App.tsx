de4a7e5e6fc3c35e85d16a7fdfa04bd613007f67
홍현아 <hyeonahong@gmail.com>
2022-07-14 17:03:14 +0900

    GlobalStyle setting, coin list css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,83 @@
 import React from 'react';
 import Router from './Router';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+//reset css 넣든 넣어주면 됨
+@import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300;400&display=swap');
+
+html, body, div, span, applet, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+a, abbr, acronym, address, big, cite, code,
+del, dfn, em, img, ins, kbd, q, s, samp,
+small, strike, strong, sub, sup, tt, var,
+b, u, i, center,
+dl, dt, dd, menu, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, embed,
+figure, figcaption, footer, header, hgroup,
+main, menu, nav, output, ruby, section, summary,
+time, mark, audio, video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article, aside, details, figcaption, figure,
+footer, header, hgroup, main, menu, nav, section {
+  display: block;
+}
+/* HTML5 hidden-attribute fix for newer browsers */
+*[hidden] {
+    display: none;
+}
+body {
+  line-height: 1;
+}
+menu, ol, ul {
+  list-style: none;
+}
+blockquote, q {
+  quotes: none;
+}
+blockquote:before, blockquote:after,
+q:before, q:after {
+  content: '';
+  content: none;
+}
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+//요것도 당연히 추가!
+* {
+  box-sizing: border-box;
+}
+body {
+  //만약 로드가 되지 않을 경우 이 폰트 쓰게 설정
+  font-family: 'Source Sans Pro', sans-serif;
+  background-color:${(props) => props.theme.bgColor};
+  color:${(props) => props.theme.textColor}
+}
+a {
+  //a tag 밑줄 없애기
+  text-decoration:none;
+  //color 부모한테 상속해줘!
+  color: inherit;
+}
+`;
 
 function App() {
-    return <Router />;
+    return (
+        <>
+            <GlobalStyle />
+            <Router />
+        </>
+    );
 }
 
 export default App;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,7 +1,83 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+const Container = styled.main`
+    padding: 0 1.25rem;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const CoinsList = styled.ul``;
+
+const Coin = styled.li`
+    background-color: white;
+    color: ${(props) => props.theme.bgColor};
+    margin-bottom: 0.625rem;
+    border-radius: 15px;
+    padding: 1.25rem;
+
+    &:hover {
+        color: ${(props) => props.theme.accentColor};
+        transition: color 0.2s ease-in;
+    }
+`;
+
+const Title = styled.h1`
+    font-size: 3rem;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const coins = [
+    {
+        id: 'btc-bitcoin',
+        name: 'Bitcoin',
+        symbol: 'BTC',
+        rank: 1,
+        is_new: false,
+        is_active: true,
+        type: 'coin',
+    },
+    {
+        id: 'eth-ethereum',
+        name: 'Ethereum',
+        symbol: 'ETH',
+        rank: 2,
+        is_new: false,
+        is_active: true,
+        type: 'coin',
+    },
+    {
+        id: 'hex-hex',
+        name: 'HEX',
+        symbol: 'HEX',
+        rank: 3,
+        is_new: false,
+        is_active: true,
+        type: 'token',
+    },
+];
 
 function Coins() {
-    return <h1>Coins</h1>;
+    return (
+        <Container>
+            <Header>
+                <Title>코인</Title>
+            </Header>
+            <CoinsList>
+                {coins.map((coin) => (
+                    <Link to={`/${coin.id}`} key={coin.id}>
+                        <Coin>{coin.name} &rarr;</Coin>
+                    </Link>
+                ))}
+            </CoinsList>
+        </Container>
+    );
 }
 
 export default Coins;
