5f1d97ae5747e8a347b49b35c1942b345543b7d2
홍현아 <hyeonahong@gmail.com>
2022-07-15 12:56:21 +0900

    Link to 를 object로 받아와서 state 가져오기

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,17 +1,58 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useState } from 'react';
+import { useParams, useLocation } from 'react-router';
+import styled from 'styled-components';
+
+const Container = styled.main`
+    padding: 0 1.25rem;
+    max-width: 30rem;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Title = styled.h1`
+    font-size: 3rem;
+    color: ${(props) => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    display: inline-block;
+    width: 100%;
+    text-align: center;
+`;
 
 //이제부터 들여야 하는 습관 : TS는 늘 설명을 해줘야 한다.
-
 //interface로 설명해주기
 interface RouteParams {
     coinId: string;
 }
 
+interface RouterState {
+    name: string;
+}
+
 function Coin() {
+    const [loading, setLoading] = useState(true);
     //TS : 그래서 const 어쩌구가 뭔데? 나 : <RouteParams> 이거야!
     const { coinId } = useParams<RouteParams>();
-    return <h1>Coin {coinId} </h1>;
+    //coins component에서 Link to로 전달된 object를 받아옴
+    //코인의 name을 이미 가지고 있어서 API가 줄 때까지 기다릴 필요가 없어짐
+    const { state } = useLocation<RouterState>();
+
+    return (
+        <Container>
+            <Header>
+                {/* object로 state를 받아왔을 때 바로 상세링크로 가면 당연히 로드된 게 없으므로 undefined 처리가 됨. 그래서 아래와 같은 트릭을 사용 */}
+                <Title>{state?.name || 'Loading...'}</Title>
+            </Header>
+            {loading ? <Loader>Loading...</Loader> : null}
+        </Container>
+    );
 }
 
 export default Coin;
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -24,10 +24,16 @@ const Coin = styled.li`
     margin-bottom: 0.625rem;
     border-radius: 15px;
     padding: 1.25rem;
+    display: flex;
+    align-items: center;
 
     &:hover {
         color: ${(props) => props.theme.accentColor};
         transition: color 0.2s ease-in;
+    }
+
+    & > p {
+        margin: 0.3rem 0 0 0.4rem;
     }
 `;
 
@@ -40,6 +46,11 @@ const Loader = styled.span`
     display: inline-block;
     width: 100%;
     text-align: center;
+`;
+
+const Img = styled.img`
+    width: 2.5rem;
+    height: 2.5rem;
 `;
 
 //데이터에 대한 타입 지정
@@ -58,9 +69,9 @@ function Coins() {
     const [coins, setCoins] = useState<CoinObject[]>([]);
     const [loading, setLoading] = useState(true);
 
-    //해당 컴포넌트에 접근할 때 불러와야 하므로 끝에 []를 넣음
+    //해당 컴포넌트에 접근할 때마다 불러와야 하므로 끝에 []를 넣음
     useEffect(() => {
-        //즉시실행함수
+        //즉시실행함수 : 한 번의 실행만 필요로 할 때 적합
         (async () => {
             const response = await fetch('https://api.coinpaprika.com/v1/coins');
             const json = await response.json();
@@ -79,8 +90,18 @@ function Coins() {
             ) : (
                 <CoinsList>
                     {coins.map((coin) => (
-                        <Link to={`/${coin.id}`} key={coin.id}>
-                            <Coin>{coin.name} &rarr;</Coin>
+                        //object 형태로 사용 가능. 데이터를 전달하는 게 가능해졌다.
+                        <Link
+                            to={{
+                                pathname: `/${coin.id}`,
+                                //다른 화면으로 state를 보내게 됨
+                                state: { name: coin.name },
+                            }}
+                            key={coin.id}>
+                            <Coin>
+                                <Img src={`https://coinicons-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`} alt={`${coin.symbol}`} />
+                                <p>{coin.name} &rarr;</p>
+                            </Coin>
                         </Link>
                     ))}
                 </CoinsList>
